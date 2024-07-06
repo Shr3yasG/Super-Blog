36650e6180e2c50fb22dbb8604c33fd5b5bb85cb
Shreyas Gawade <shreyaa1110@gmail.com>
2024-07-06 13:33:40 +0530

    Day-4

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
-export  { Header, Footer, Container, Logo,LogoutBtn };
+import RTE from "./RTE";
+import Signup from "./Signup"
+export  { Header, Footer, Container, Logo,LogoutBtn, RTE ,Signup};
